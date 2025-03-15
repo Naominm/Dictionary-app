c70179f51a0a276e1c1251060ba9a9159da73658
Naominm <naomimbugua536@gmail.com>
2025-03-15 23:51:40 +0300

    Initial Project Reset

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Dictionary from "./components/Dictionary";
+
+function App(){
+  return(
+    <>
+    <h1>Hello there this is a dictionary app</h1>
+<Dictionary/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Dictionary.jsx
@@ -0,0 +1,12 @@
+
+
+import "./Dictionary.css";
+function Dictionary(){
+    return(
+        <div className="main-content">
+          <p> No! this is the actual dictionary</p>
+        </div>
+    )
+}
+
+export default Dictionary;
