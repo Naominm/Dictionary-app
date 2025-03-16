a009dc65224885ff858a2dda01e78b9c73d3347b
Naominm <naomimbugua536@gmail.com>
2025-03-16 10:21:55 +0300

    Build the interface

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Dictionary from "./components/Dictionary";
 function App(){
   return(
     <>
-    <h1>Hello there this is a dictionary app</h1>
 <Dictionary/>
     </>
   )
--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,43 +1,62 @@
 
 import {useState,useEffect}  from 'react'
+
 import "./Dictionary.css";
 
 function Dictionary(){
-const[wordMeaning, setWordMeaning]=useState(null);
-const[error,setError]=useState('');
-const [loading, setLoading] = useState(true);
+// const[wordMeaning, setWordMeaning]=useState(null);
+// const[error,setError]=useState('');
+// const [loading, setLoading] = useState(true);
 
-useEffect(()=>{
+// useEffect(()=>{
 
-    async function GetMeaning(){
-        try {
-        const response=await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/hello`)
-        if(!response.ok){
-           throw new Error("failed to fetch");
-        }
-        const data=await response.json();
-        setWordMeaning(data[0]);
-} catch (error) {
-    setError("Error Fetching the meaning please try again later")
-   }
-  finally{
-    setLoading(false);
-  }
-}
-GetMeaning()
+//     async function GetMeaning(){
+//         try {
+//         const response=await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/hello`)
+//         if(!response.ok){
+//            throw new Error("failed to fetch");
+//         }
+//         const data=await response.json();
+//         setWordMeaning(data[0]);
+// } catch (error) {
+//     setError("Error Fetching the meaning please try again later")
+//    }
+//   finally{
+//     setLoading(false);
+//   }
+// }
+// GetMeaning()
 
-}, []);
+// }, []);
 
-if(loading)return <p>loading...</p>
+// if(loading)return <p>loading...</p>
 
-if(error)return <p className='error'>{error}</p>
+// if(error)return <p className='error'>{error}</p>
 
-if(!wordMeaning) return <p>No data found</p>
+// if(!wordMeaning) return <p>No data found</p>
 
 return(
+  <div className="parent">
     <div className="main-content">
-   <h1>{wordMeaning.word}</h1>
+      <form className='form-elements'>
+        
+          <h2 className='title'>Welcome to Dictionary</h2>
+          <div className="col-elements">
+        <input type="text" className='search' placeholder='Enter the word here to get the meaning' />
+        <button className='search submit'>submit</button>
+      
+        </div>
+      </form>
+
+   {/* <h1>{wordMeaning.word}</h1> */}
     
+</div>
+<div className="result-content">
+  <p>lets get the meaning</p>
+  <div className="word-title"></div>
+  <div className="word-audio"></div>
+  <div className="word-synonym"></div>
+</div>
 </div>
     )
 }
